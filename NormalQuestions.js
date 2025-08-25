ea3c787560ac68bde169a3aa1d4272a7fa2a65a3
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-25 21:21:07 +0530

    Added the notes where i stated all the types of loops and there uses

---
+++ NormalQuestions.js
@@ -0,0 +1,5 @@
+// For loop - runs code a fixed number of times
+// While - loop runs the code until the condtion is doesnt change to true or false
+// Do while - runs the code atleast once, then checks the condition
+// For of loop - Loops that are made for arrays, Makes accessing data from an array quite easier
+// For in loop - It is specifically for looping through an objects data, With parameters like (key and index)
