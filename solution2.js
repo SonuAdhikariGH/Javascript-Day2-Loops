ac6e4ae1a314ca46dda2d32a6586468122829989
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-25 21:46:52 +0530

    Solved second question where i had to make a logic of printing only even number in the console using while loop

--- solution2.js
+++ solution2.js
@@ -1,1 +1,11 @@
 // Writing the snippet as usual and doing most;y the same thing as for loops but with a different tweak
+
+let i = 2;
+// Declared variable for storing value
+while(i <= 20) {
+    // We must declare the variable outside of while using "While loop"
+    console.log(i)
+    // Loging the result to console to debug
+    i += 2;
+    // Adiing += will store and increase the value so instead of using + only will give you an infinite loop
+}
